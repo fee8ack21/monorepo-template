53fdd7f2c6e668a122e9ab7bcf50d3ce387a3edd
Pin Jui Huang <fee8ack21@gmail.com>
2024-08-16 00:14:17 +0800

    perf: 調整 app1 專案 tailwind.config

--- apps/app1/tailwind.config.js
+++ apps/app1/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
     content: [
         "./src/**/*.{vue,js,ts,jsx,tsx}",
-        "../../packages/components/**/*.{vue,js,ts,jsx,tsx}",
+        "../../packages/components/src/**/*.{vue,js,ts,jsx,tsx}",
     ],
     theme: {
         colors: {
