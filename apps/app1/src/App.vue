bcc47fbfed5340b391f83cea7c9b8424ff497823
Pin Jui Huang <fee8ack21@gmail.com>
2024-08-16 00:17:08 +0800

    feat: 調整 app 專案 html title

--- apps/app1/src/App.vue
+++ apps/app1/src/App.vue
@@ -1,19 +1,16 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
-import { OpenAPI } from "@monorepo-template/api";
 import { AccountService } from "@monorepo-template/api";
-import { add } from "@monorepo-template/utilities";
+import { add, uuidv4 } from "@monorepo-template/utilities";
 import { BaseButton, BaseLogo } from "@monorepo-template/components";
 
-OpenAPI.BASE = import.meta.env.VITE_API_BASE_URL;
-
 onMounted(() => console.log(add(1, 2)));
+onMounted(() => console.log(uuidv4()));
 onMounted(async () => await AccountService.getAccounts());
 </script>
 
 <template>
-    <BaseLogo />
-    <!-- <br/> -->
-    <BaseButton />
-    <p class="test">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, natus.</p>
+  <BaseLogo />
+  <!-- <br/> -->
+  <BaseButton />
 </template>
--- apps/app1/src/main.ts
+++ apps/app1/src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import '@/assets/style/index.scss'
 import App from './App.vue'
 
+// define api url
+import { OpenAPI } from "@monorepo-template/api";
+OpenAPI.BASE = import.meta.env.VITE_API_BASE_URL;
+
 // plugins
 import '@/plugins/axios'
 
