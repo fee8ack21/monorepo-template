82a6000f7bc006c106489cc390c62ba23f07fbd3
fee8ack21 <fee8ack21@gmail.com>
2024-08-24 11:21:47 +0800

    fix: 調整專案設定檔引用方式

--- apps/app2/tailwind.config.js
+++ apps/app2/tailwind.config.js
@@ -1,24 +1,10 @@
-const { baseContent, baseConfig } = require('../../tailwind.base.config.js')
-
+const { baseContent, baseScreens, baseConfig } = require('../../tailwind.base.config.js')
 /** @type {import('tailwindcss').Config} */
 export default {
   presets: [baseConfig],
   content: ['./src/**/*.{vue,js,ts,jsx,tsx}'].concat(baseContent),
   theme: {
-    colors: {
-      primary: {
-        50: '#FFF5E6',
-        100: 'green',
-        200: '#FFD495',
-        300: '#FFC875',
-        400: '#FFB74C',
-        500: '#FFAD34',
-        600: '#FF9801',
-        700: '#F37D27',
-        800: '#692601'
-      }
-    },
-    extend: {}
+    screens: baseScreens
   },
   plugins: []
 }
--- tailwind.base.config.js
+++ tailwind.base.config.js
@@ -1,21 +1,13 @@
 /** 基於引用處的相對路徑 */
 export const baseContent = ['../../packages/ui/src/**/*.{vue,js,ts,jsx,tsx}']
 
-/** @type {import('tailwindcss').Config} */
-export const baseConfig = {
-  theme: {
-    colors: {
-      primary: {
-        50: '#FFF5E6',
-        100: 'green',
-        200: '#FFD495',
-        300: '#FFC875',
-        400: '#FFB74C',
-        500: '#FFAD34',
-        600: '#FF9801',
-        700: '#F37D27',
-        800: '#692601'
-      }
-    }
-  }
+export const baseScreens = {
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+  '2xl': '1536px'
 }
+
+/** @type {import('tailwindcss').Config} */
+export const baseConfig = {}
